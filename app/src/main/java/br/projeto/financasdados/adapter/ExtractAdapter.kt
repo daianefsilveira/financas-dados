3aa42c9c50ca69fe4f56060a7afee8ec67310c25
dsilveira <daiane.silveira@foursys.com.br>
2022-11-29 16:19:11 -0300

    Adding recycler view and adapter class

---
+++ app/src/main/java/br/projeto/financasdados/adapter/ExtractAdapter.kt
@@ -0,0 +1,58 @@
+package br.projeto.financasdados.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import br.projeto.financasdados.R
+import br.projeto.financasdados.databinding.TransactionItemBinding
+import br.projeto.financasdados.model.Transaction
+
+class ExtractAdapter(var transactionsList: List<Transaction>, private val listener: onItemClickListener) :
+    RecyclerView.Adapter<ExtractAdapter.ExtractViewHolder>() {
+
+    interface onItemClickListener {
+        fun delete(position: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExtractViewHolder {
+        val binding = TransactionItemBinding
+            .inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        return ExtractViewHolder(binding)
+    }
+
+    override fun getItemCount() = transactionsList.size
+
+    override fun onBindViewHolder(holder: ExtractViewHolder, position: Int) {
+
+        holder.binding.ivRemove.setOnClickListener {
+            listener.delete(position)
+        }
+        holder.bind(transactionsList[position])
+    }
+
+    class ExtractViewHolder(val binding: TransactionItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(transaction: Transaction) = with(itemView) {
+            binding.tvDescriptionItem.text = transaction.description
+            binding.tvDate.text = transaction.date
+
+            if (transaction.savedMoney) {
+                binding.tvAmount.text = String.format("R$ %.2f", transaction.amount)
+                binding.card.setCardBackgroundColor(ContextCompat.getColor(context, R.color.green))
+                binding.card.setCardBackgroundColor(ContextCompat.getColor(context, R.color.saved_money))
+            } else {
+                binding.tvAmount.text = String.format("R$ -%.2f", transaction.amount)
+                binding.tvAmount.setTextColor(ContextCompat.getColor(context, R.color.red))
+                binding.card.setCardBackgroundColor(ContextCompat.getColor(context, R.color.spent_money))
+            }
+        }
+    }
+}
