3e43b5d2df3a1db4c57d352b45f684c17d1c4ff6
dsilveira <daiane.silveira@foursys.com.br>
2022-11-29 16:13:16 -0300

    Adding fragments, activity main and layouts XML

--- app/src/main/java/br/projeto/financasdados/MainActivity.kt
+++ app/src/main/java/br/projeto/financasdados/MainActivity.kt
@@ -2,10 +2,16 @@ package br.projeto.financasdados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import br.projeto.financasdados.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
+
